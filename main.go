a275c99663a7345353f87969b6a6fc8e551d348d
Sumit Parida <80693203+sumitparida2002@users.noreply.github.com>
2024-05-07 17:13:30 -0400

    refactored

--- main.go
+++ main.go
@@ -2,78 +2,52 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strconv"
 	"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
-		run()
+		parent()
 	case "child":
 		child()
-
 	default:
-		panic("bad command")
+		panic("wat should I do")
 	}
 }
 
-func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
-
+func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
 	}
-
-	cmd.Run()
-
 }
 
 func child() {
-	fmt.Printf("Running %v\n", os.Args[2:])
-
-	syscall.Sethostname([]byte("container"))
-	syscall.Chroot("/") //Define your own
-	syscall.Chdir("/")
-
-	syscall.Mount("proc", "proc", "proc", 0, "")
+	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
+	must(os.MkdirAll("rootfs/oldrootfs", 0700))
+	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+	must(os.Chdir("/"))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-		UnshareFlags: syscall.CLONE_NEWNS,
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
 	}
-
-	cmd.Run()
-
-	syscall.Unmount("proc", 0)
-
-}
-
-func cg() {
-	cgroups := "/sys/fs/cgroups/"
-	pids := filepath.Join(cgroups, "pids")
-	err := os.Mkdir(filepath.Join(pids, "liz"), 0755)
-	if err != nil && !os.IsExist(err) {
-		panic(err)
-	}
-
-	must(ioutil.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
-	must(ioutil.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
-	must(ioutil.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
-
 }
 
 func must(err error) {
